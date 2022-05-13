e002952a16f00fb0c53586e208513408b41a13ef
Bouhm-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-13 13:23:27 +0100

    cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask_restx import Api
 from flask import Blueprint
 
-from .main.controller.user_controller import api as user_ns
-from .main.controller.auth_controller import api as auth_ns
 
 blueprint = Blueprint('api', __name__)
 authorizations = {
@@ -22,5 +20,5 @@ api = Api(
     security='apikey'
 )
 
-api.add_namespace(user_ns, path='/user')
-api.add_namespace(auth_ns)
+# api.add_namespace(user_ns, path='/user')
+# api.add_namespace(auth_ns)
